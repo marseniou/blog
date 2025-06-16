61dd0008ecd25384aeb37a2f3b0a65447c41f2fa
Marios Arseniou <marseniou@gmail.com>
2025-06-16 12:29:52 +0300

    added my-8 to h1

--- resources/views/components/members/grid.blade.php
+++ resources/views/components/members/grid.blade.php
@@ -2,7 +2,7 @@
     {{-- < class="px-6 py-12 sm:py-28"> --}}
     <div class="container mx-auto md:max-w-screen-md lg:max-w-screen-lg px-8 py-4">
         {{ Breadcrumbs::render('members') }}
-        <h1 class="text-3xl">@lang('messages.members')</h1>
+        <h1 class="text-3xl my-8">@lang('messages.members')</h1>
         <div class="bg-gradient-to-b from-base-100 to-base-200 rounded-lg">
 
             <div class="grid gap-10 lg:gap:20 md:grid-cols-2 xl:grid-cols-3">
