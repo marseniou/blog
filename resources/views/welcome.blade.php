4b309fc10fc63650ebefb2a6a129c4f3beeb5ba0
Marios Arseniou <marseniou@gmail.com>
2025-11-12 22:22:11 +0200

    logo in front page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,21 @@
 <x-layouts.main>
     <div class="hero bg-base-200 min-h-screen">
         <div class="hero-content text-center">
-            <div class="max-w-lg">
 
-                <h1 class="text-4xl">
+            <div class="max-w-lg">
+                <div class="flex justify-center">
+                    <img class="rounded-lg overflow-hidden mb-8" src="/logos/archart.jpg" alt="Archart Project">
+                </div>
+                <h1 class="text-4xl mb-4">
                     @lang('messages.front')
                 </h1>
-                <h3 class="text-2xl font-bold py-8 text-primary">ArchArt Project</h3>
+                {{-- <h3 class="text-2xl font-bold py-8 text-primary">ArchArt Project</h3> --}}
+
                 @if (App\Models\Page::where('slug', 'front-page')->exists())
                     <x-helpers.front-page />
                 @endif
-                <a href="{{ route('page.single', ['page' => 'summary']) }}" class="btn btn-primary mb-4">@lang('messages.summary')</a>
+                <a href="{{ route('page.single', ['page' => 'summary']) }}"
+                    class="btn btn-primary mb-4">@lang('messages.summary')</a>
                 <a href="https://engonopoulos.gr" target="_blank" class="btn btn-primary mb-4">@lang('messages.episimo')</a>
                 </a>
 
